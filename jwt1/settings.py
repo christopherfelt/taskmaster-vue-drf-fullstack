94446e5f43ccdca83af5c86b6e60a5c7235fb0ca
chris_MSI1 <chris.h.felt@gmail.com>
2020-08-12 15:22:17 -0600

    back to fixing settings

--- jwt1/settings.py
+++ jwt1/settings.py
@@ -130,12 +130,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOW_CREDENTIALS = True
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:8080',
     'http://127.0.0.1:8000',
-    'http://taskmaster-vue-drf.herokuapp.com'
+    'https://taskmaster-vue-drf.herokuapp.com'
 )
 
 REST_FRAMEWORK = {
